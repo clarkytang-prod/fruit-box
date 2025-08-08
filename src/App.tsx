5c1681515f4d4e3a3bc3288859db0262ff48256b
clarkytang-prod <clark.y.tang@gmail.com>
2025-08-08 16:44:35 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import FruitBox from "./FruitBox";
+
+export default function App() {
+  return <FruitBox />;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+// Tailwind v4 note: basic usage works with just @import "tailwindcss"; in index.css.
+// You can extend theme or plugins here later if you want.
+export default {
+}
